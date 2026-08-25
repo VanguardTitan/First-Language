07a3e2696cc60242f3c1d2e578c0db5a138767f5
VanguardTitan <divyanshu7781174@gmail.com>
2026-08-25 21:24:02 +0530

    Hello world and sum

---
+++ hello_world.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    int a = 56, b = 11;
+    int sum = a + b;
+    float pi = 3.14;
+    char c = 'd';
+    
+    cout<<"Hello World. \nThis is tutorial 1. \nThe value of x is "<<a<<". And value of z is "<<b;
+
+    cout<<"\nThe sum of x and z is "<<sum;
+
+    cout<<"\nThe value of pi is "<<pi;
+
+    cout<<"\nI want to switch the value of y to "<<c;
+
+
+    return 0;
+}
