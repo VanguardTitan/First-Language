ad56b84dfee2363c5fef8bedc763efeddbaead42
VanguardTitan <divyanshu7781174@gmail.com>
2026-08-26 12:43:18 +0530

    Add files via upload

---
+++ 01_if_else.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+ int age;
+
+cout<<"Write your age \n";
+cin>> age;
+
+if (age>=18){
+    cout<<"You are Allowed to Enter";
+} else{
+    cout<<"You are NOT Allowed to Enter";
+}
+
+
+
+// char ch;
+// cout<<"Tell me your alphabet : \n";
+// cin>>ch;
+
+// if(ch>='a' && ch<'z'){
+//     cout<<"The Entered alphabet is small";
+// } else{
+//     cout<<"The Entered alphabet is capital" ;
+// }  
+
+    return 0;
+}
