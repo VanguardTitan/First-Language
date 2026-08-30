450c1a6872db0839d46074316d1f3f187cd3dbd0
Divyanshu <divyanshu7781174@gmail.com>
2026-08-30 08:25:21 +0530

    simple square pattern and calculator

---
+++ arithmatic_01.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main (){
+
+int a, b;
+cout<<"Enter A:";
+cin>>a;
+cout<<"Enter B:";
+cin>>b;
+
+int sum = a+b;
+int difference = a-b;
+int product = a*b;
+int division = a/b;
+
+
+cout<<"Your sum is:"<<sum<<"\n";
+cout<<"Your Difference is:"<<difference<<"\n";
+cout<<"Your product is:"<<product<<"\n";
+cout<<"Your division is:"<<division<<"\n";
+
+
+
+
+    return 0;
+}
---
+++ patterns_01.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+int n = 5;
+    int num = 1;
+
+    for(int i = 0; i < n; i++){
+    for(int j = 1; j <= n; j++){
+    cout<<num<<" ";
+    num++;      
+}
+
+    cout<<"\n"; 
+    }
+
+
+int d = 5;
+char ch ='a';
+
+for(int i = 0; i < d; i++){
+    for(int j = 0; j < d; j++){
+        cout<< ch<<" " ;
+        ch++;
+    }
+    cout<<"\n";
+}
+
+
+
+    
+    return 0;
+}
