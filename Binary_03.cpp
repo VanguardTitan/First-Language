69035a25aced14ac3c3e0009fddde721eaa53b4b
Divyanshu <divyanshu7781174@gmail.com>
2026-09-07 22:52:29 +0530

    Check Bitwise operators

---
+++ Binary_03.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+// checking the bitwise operators 
+
+int a = 5, b = 10;
+cout<<( a & b )<<endl;
+cout<<( a | b )<<endl;
+cout<<( a ^ b )<<endl;
+
+
+// Checking the Data modifiers 
+
+cout<< sizeof(int)<<endl;
+cout<< sizeof(long long int)<<endl;
+    return 0;
+}
